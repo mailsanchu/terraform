3374a1560b95cf45c7e93aab9fc597e50599c125
mailsanchu <32928538+mailsanchu@users.noreply.github.com>
2018-06-05 17:21:06 +0100

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,47 @@
+output "address" {
+  value = "${aws_elb.web.dns_name}"
+}
+output "id" {
+  description = "List of IDs of instances"
+  value = [
+    "${aws_instance.SanchuTest.*.id}"]
+}
+output "public_ip" {
+  description = "List of public_ip's of instances"
+  value = [
+    "${aws_instance.SanchuTest.*.public_ip}"]
+}
+
+
+/*
+output "id" {
+  description = "List of IDs of instances"
+  value = [
+    "${aws_instance.SanchuTest.*.id}"]
+}
+
+output "availability_zone" {
+  description = "List of availability zones of instances"
+  value = [
+    "${aws_instance.SanchuTest.*.availability_zone}"]
+}
+
+
+output "key_name" {
+  description = "List of key names of instances"
+  value = [
+    "${aws_instance.SanchuTest.*.key_name}"]
+}
+output "public_dns" {
+  description = "List of public DNS names assigned to the instances. For EC2-VPC, this is only available if you've enabled DNS hostnames for your VPC"
+  value = [
+    "${aws_instance.SanchuTest.*.public_dns}"]
+}
+
+output "public_ip" {
+  description = "List of public IP addresses assigned to the instances, if applicable"
+  value = [
+    "${aws_instance.SanchuTest.*.public_ip}"]
+}
+*/
+
