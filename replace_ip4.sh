9bf064fc67cc3495e2c9f0b340d84f9a7e6baae7
Sanchu Varkey <svarkey@digisoft.tv>
2018-06-06 12:27:44 +0100

    More script changes

--- replace_ip4.sh
+++ replace_ip4.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+sudo yum -y update
+sudo yum -y install nginx
+sudo service nginx start
 IP_ADDR=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 echo $IP_ADDR
 sudo sed -Ei "s/Amazon Linux AMI/Amazon Linux AMI-$IP_ADDR/g" /usr/share/nginx/html/index.html
--- wait_for_elb.sh
+++ wait_for_elb.sh
@@ -2,7 +2,7 @@
 echo "Waiting for LOAD_BAL_DNS ... "
 while true;
 do
-	HTTP_STATUS=`wget --no-check-certificate --spider -S "http://LOAD_BAL_DNS" 2>&1 | grep "HTTP/" | awk '{print $2}'`
+	HTTP_STATUS=`wget --no-check-certificate --spider -S "http://LOAD_BAL_DNS" | grep "HTTP/" | awk '{print $2}'`
 	if [[ $HTTP_STATUS = "200" ]]; then
 		echo Status is 200 OK, Application is Up!;
 		break;
